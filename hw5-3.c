891e4d52c2b42e50f413cf1213305ee99eb67086
weijun0705 <146065151+weijun0705@users.noreply.github.com>
2023-11-06 21:40:04 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,19 @@
 
+int main()
+{float  a,b,c;
+scanf("%f%f",&a,&b);
+if(a<=60)
+{
+    c=(double)b*a;
+}
+else if(a>60&&a<=120)
+{
+c=(double)b*60+b*(a-60)*1.33;
+}
+else if (a>120)
+{
+  c=(double)b*60+b*60*1.33+b*(a-120)*1.66;
+}
+
+printf("%.1f",c);
+}
