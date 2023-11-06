a86b89ca3c5c3aea9703bf80c528eeb83b3ed4d6
weijun0705 <146065151+weijun0705@users.noreply.github.com>
2023-11-06 22:06:34 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{float  a,b,c,d;
+scanf("%f%f",&a,&b);
+c=(double)a*360/12+b*0.5-b*360/60;
+if(c<0)
+{
+    d=(double)c+180;
+}
+else if(c>=0&&c<=180)
+{
+d=c;
+}
+else if (c>180)
+{
+  d=(double)c-180;
+}
+
+printf("%.3f",d);
+}
